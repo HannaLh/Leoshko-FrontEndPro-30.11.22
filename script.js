1f9de050fa56f8d5591c59b2e0a2c8cc767bea27
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-30 17:52:16 +0100

    implemented convertation from hours to seconds

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+const hours = +prompt('Input hours here');
 
-switch(Boolean(numOrStr) || numOrStr) {     //або switch(!!numOrStr || numOrStr)
-    case null:
-        console.log('ви скасували');
-        break;
-    case '':
-        console.log('Empty String');
-        break;
-    case isNaN(+numOrStr):
-        console.log('number is Ba_NaN');
-        break;
-    default:
-        console.log('OK!');
+if (!!Number(hours)) { // typeof hours === 'number'
+    const seconds = hours * 60;
+    alert(seconds);
+} else {
+    alert('Please, enter the NUMBER!');
 }
