f6b700b2832a01d01bfff18c548d069910d32681
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-30 18:28:00 +0100

    implemented calculating program

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = +prompt('Enter the first number');
+let num2 = +prompt('Enter the second number'); 
+ 
+alert(num1 + " * " + num2 + " = " + `${num1 * num2}`); 
+alert(num1 + " / " + num2 + " = " + `${num1 / num2}`); 
+alert(num1 + " + " + num2 + " = " + `${num1 + num2}`); 
+alert(num1 + " - " + num2 + " = " + `${num1 - num2}`);
