82c58c43432e89d1799a8c730765e1e51094e6e6
Anna Leoshko <anna.leoshko@gmail.com>
2022-12-09 08:39:39 +0100

    refactoring code

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ console.log(result);
 //Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
 
 for (let i = 10; i <= 100; ) {
-    let UAH = 27;
-    UAH = `${i} гривень = ${i / UAH} доларів`;
-    console.log(UAH);
+    let uahPerDollar = 27;
+    uahPerDollar = `${i} гривень = ${i / uahPerDollar} доларів`;
+    console.log(uahPerDollar);
     i += 10;
 }
 
@@ -30,31 +30,30 @@ for(let i = 1; i <= 100; i++) {
 
 //Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 
-const number = +prompt('Enter any integer bigger than 1');
+const n = +prompt('Enter any integer bigger than 1');
 
-if (number < 2) {
+if (n < 2) {
     alert("Enter the number greater than 1");
 }
 
-stop:if (number > 1) {
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0) {
-            alert(`${number} isn't a prime number`);
+stop:if (n > 1) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            alert(`${n} isn't a prime number`);
             break stop;
         }
     }
-    alert(`${number} is a prime number`);
+    alert(`${n} is a prime number`);
 }
-
 
 //Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
 
-let num = +prompt('Enter any positive number');
+let n = +prompt('Enter any positive number');
 let i = 1;
 let powerOfThree = 3 ** i;
 
-while (powerOfThree <= num) {
-    if (num === powerOfThree) {
+while (powerOfThree <= n) {
+    if (n === powerOfThree) {
         alert('We got this number by raising 3 to the power');
         break;
     }
