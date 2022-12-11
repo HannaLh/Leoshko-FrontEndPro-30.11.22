aa752654a871511902e64997388f927b73f50c59
Anna Leoshko <anna.leoshko@gmail.com>
2022-12-11 17:32:50 +0100

    refactoring code

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 
 let arr = [16,-37, 54, -4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let sumOfPositivDigits = 0,
-positiveNum = 0,
-negativeNum = 0,
-oddNegativeNum = 0,
-oddPositiveNum = 0,
-sumOddPositiveNum = 0,
-sumOddNegativeNum = 0,
-prodPositivNum = 1,
+
+let sumPositivNum = 0,
+amountPositiveNum = 0,
+
+minNum = 0,
+minNumIndex = 0,
 
 maxNum = 0,
 maxNumIndex = 0,
 
-minNum = 0,
-minNumIndex = 0,
+amountNegativeNubmers = 0,
+
+amountOddPositiveNum = 0,
+
+evenPositiveNumAmount = 0,
+sumEvenPositiveNum = 0,
+
+sumOddPositiveNum = 0,
+
+prodPositiveNum = 1,
 
 maxNumberIndex = 0;
 
 
 for (let i = 0; i < arr.length; i++) {
     if(arr[i] > 0) {
-        sumOfPositivDigits += arr[i];
-        positiveNum += 1;
-        prodPositivNum *= arr[i];
+        sumPositivNum += arr[i];
+        amountPositiveNum += 1;
+        prodPositiveNum *= arr[i];
 
         if (maxNum < arr[i]) {
             maxNum = arr[i];
@@ -30,17 +36,16 @@ for (let i = 0; i < arr.length; i++) {
         }
 
         if(arr[i] % 2 === 0) {
-            oddPositiveNum += 1;
+            evenPositiveNumAmount += 1;
+            sumEvenPositiveNum += arr[i];
+        }
+        if(arr[i] % 2 === 1) {
+            amountOddPositiveNum += 1;
             sumOddPositiveNum += arr[i];
         }
     }
     if(arr[i] < 0) {
-        negativeNum += 1;
-
-        if(arr[i] % 2 === -1) {
-            oddNegativeNum += 1;
-            sumOddNegativeNum += arr[i];
-        }
+        amountNegativeNubmers += 1;
 
         if (minNum > arr[i]){
             minNum = arr[i];
@@ -56,12 +61,13 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
-console.log(`1. Positive elements: ${sumOfPositivDigits}; Positive amount: ${positiveNum}`);
-console.log(`2. Min number in array is ${minNum}; Min number index: ${minNumIndex}`);
-console.log(`3. Max number in array is ${maxNum}; Max number index: ${maxNumIndex}`);
-console.log(oddNegativeNum);
-console.log(oddPositiveNum);
-console.log(`Min element in array: ${negativeNum}, `);
-console.log(sumOddPositiveNum);
-console.log(sumOddNegativeNum);
-console.log(prodPositivNum);
+console.log(`1. Positive elements: ${sumPositivNum}; Positive amount: ${amountPositiveNum}`);
+console.log(`2. Min number in array: ${minNum}; Min number index: ${minNumIndex}`);
+console.log(`3. Max number in array: ${maxNum}; Max number index: ${maxNumIndex}`);
+console.log(`4. Amount negative number in array: ${amountNegativeNubmers}`);
+console.log(`5. Amount odd positive number in array: ${amountOddPositiveNum}`);
+console.log(`6. Amount even positive number in array: ${evenPositiveNumAmount}`);
+console.log(`7. Sum of even positive number in array: ${sumEvenPositiveNum}`);
+console.log(`8. Sum of odd positive numbers: ${sumOddPositiveNum}`);
+console.log(`9. Product of positive numbers: ${prodPositiveNum}`);
+console.log(arr);
