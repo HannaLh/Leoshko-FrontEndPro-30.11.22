ea7f9437bd4a55f87e87510e1d2da14342f5bb2e
Anna Leoshko <anna.leoshko@gmail.com>
2022-12-14 11:45:50 +0100

    code refactoring

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const arrLength = +prompt('Enter a number, which is the length of array:');
 let arr = [];
 
-for (let num = 0; num < arr.length; num++) {
+for (let num = 0; num < arrLength; num++) {
     let digit = +(prompt('Enter digit #' + (num + 1)));
     arr.push(digit);
 }
 
 console.log(arr);
 
-arr.sort();
-console.log(arr);
+const sortedArr = arr.sort(
+    (firstEl, secondEl) => {
+        if (firstEl > secondEl) {
+            return 1;
+        } else if (firstEl < secondEl) {
+            return -1;
+        }
+        return 0;
+    }
+);
+
+arr = sortedArr;
+console.log(sortedArr);
 
 arr.splice(1,3);
 console.log(arr);
