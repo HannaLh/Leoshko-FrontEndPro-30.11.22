90312f2e28e45e35ab2535acaa142466b9fb1d35
Anna Leoshko <anna.leoshko@gmail.com>
2023-02-16 17:59:50 +0100

    implemented fetch task

---
+++ script.js
@@ -0,0 +1,34 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const post = document.getElementById('post');
+const comments = document.getElementById('postComments');
+const idInput = document.getElementById("user_input");
+
+document.querySelector("#search").addEventListener("click", () => {
+    if(validate()) {
+    const user_input = document.querySelector("#user_input");
+    request(user_input);
+    }
+})
+
+const getPostCommentsByPostId = (id) => {
+    const url = `${BASE_URL}/posts/3/comments`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => comments.textContent = '\n' + JSON.stringify(json, '\t', 4));
+}
+
+request = (user_input) => {
+    const url = `${BASE_URL}/posts/${user_input.value}`;
+    fetch(url)
+    .then(response => {
+        if (!response.ok) throw Error(response.statusText);
+            return response.json();
+    })
+    .then(data => {
+        document.querySelector("#isValid").textContent = "";
+        post.textContent = '\n' + JSON.stringify(data, '\t', 2);
+    })
+    .catch(error => document.querySelector("#isValid").textContent = error);
+
+    document.querySelector("#getComment").addEventListener("click", getPostCommentsByPostId);
+}
